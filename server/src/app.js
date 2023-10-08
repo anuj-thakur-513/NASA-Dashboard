9434da648e4a561973033e67081dffe24cd57c96
Anuj Thakur <anujthakur2003.anuj@gmail.com>
2023-10-08 23:27:28 +0530

    serving React.js front-end in production

--- server/src/app.js
+++ server/src/app.js
@@ -1,16 +1,25 @@
 const express = require("express");
+const path = require("path");
 const cors = require("cors");
 
 const planetsRouter = require("./routes/planets/planets.router");
 
 const app = express();
+const serveDirectory = path.join(__dirname, "..", "public");
 
+// Cross Origin Communication
 app.use(
   cors({
     origin: "http://localhost:3000",
   })
 );
-app.use(express.json());
+app.use(express.json()); // middleware to parse JSON
+app.use(express.static(serveDirectory)); // middleware to serve frontend
 app.use(planetsRouter);
+
+// root router
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "public", "index.html"));
+});
 
 module.exports = app;
