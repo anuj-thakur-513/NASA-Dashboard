82ac98fb99f1c6628dd2828873ed4aaddae90d8a
Anuj Thakur <anujthakur2003.anuj@gmail.com>
2023-10-10 10:14:01 +0530

    fix: serving application with client side routing

--- server/src/app.js
+++ server/src/app.js
@@ -24,7 +24,7 @@ app.use(planetsRouter);
 app.use(launchesRouter);
 
 // root router
-app.get("/", (req, res) => {
+app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "..", "public", "index.html"));
 });
 
