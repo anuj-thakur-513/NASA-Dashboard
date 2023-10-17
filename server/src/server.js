a28057641683a600901069b5d11926cc4158b28f
Anuj Thakur <anujthakur2003.anuj@gmail.com>
2023-10-17 09:59:30 +0530

    feat: connected MONGO_DB

--- server/src/server.js
+++ server/src/server.js
@@ -1,12 +1,26 @@
 const http = require("http");
+const mongoose = require("mongoose");
+
 const app = require("./app");
 const { loadPlanetsData } = require("./models/planets.model");
+
+require("dotenv").config();
+const { DB_URL } = process.env;
 
 const PORT = process.env.PORT || 8000;
 
 const server = http.createServer(app);
 
+mongoose.connection.once("open", () => {
+  console.log("MongoDB connection ready!!!");
+});
+
+mongoose.connection.on("error", (err) => {
+  console.error(err);
+});
+
 async function startServer() {
+  await mongoose.connect(DB_URL);
   await loadPlanetsData();
   server.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
